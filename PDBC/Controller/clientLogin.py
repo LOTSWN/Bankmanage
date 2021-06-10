e107e5a95efb5e8e1db2a32eac1ac486367d6b17
epc314 <513541145@qq.com>
2021-06-10 09:59:54 +0800

    管理员staff登录

--- PDBC/Controller/clientLogin.py
+++ PDBC/Controller/clientLogin.py
@@ -1,6 +1,7 @@
 from flask import Flask , request,render_template
 import json
 from Dao.ClientDao import ClientDao
+from Dao.StaffDao import StaffDao
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -15,6 +16,25 @@ def checkLogin():
 
     if diccd == None:
         print("there")
+        return "notfind"
+    if(diccd['password']==password) :
+        print("right")
+        return "find"
+    else :
+        print("wrong")
+        return "notfind"
+
+
+@app.route("/Stafflogin", methods=["GET", "POST"])
+def checkStaffLogin():
+    cd = StaffDao()
+    data = json.loads(request.form.get('data'))
+    username = data['username']
+    password = data['password']
+    diccd=cd.findstaffByID(theId=username)
+
+    if diccd == None:
+        print("ID don't exist")
         return "notfind"
     if(diccd['password']==password) :
         print("right")
